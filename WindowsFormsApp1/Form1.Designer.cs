06e1907239dbf0b0e49d95aa38a0a5d947b7a1b4
Ilyas Makhloul <127142801+ilyasMak@users.noreply.github.com>
2023-11-19 16:47:34 +0100

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B00 = new System.Windows.Forms.Button();
+            this.B01 = new System.Windows.Forms.Button();
+            this.B02 = new System.Windows.Forms.Button();
+            this.B10 = new System.Windows.Forms.Button();
+            this.B11 = new System.Windows.Forms.Button();
+            this.B12 = new System.Windows.Forms.Button();
+            this.B20 = new System.Windows.Forms.Button();
+            this.B21 = new System.Windows.Forms.Button();
+            this.B22 = new System.Windows.Forms.Button();
+            this.TBply1 = new System.Windows.Forms.TextBox();
+            this.TBply2 = new System.Windows.Forms.TextBox();
+            this.Lwinner = new System.Windows.Forms.Label();
+            this.Breplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // B00
+            // 
+            this.B00.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.B00.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B00.ForeColor = System.Drawing.Color.Red;
+            this.B00.Location = new System.Drawing.Point(254, 144);
+            this.B00.Name = "B00";
+            this.B00.Size = new System.Drawing.Size(127, 94);
+            this.B00.TabIndex = 0;
+            this.B00.UseVisualStyleBackColor = false;
+            this.B00.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // B01
+            // 
+            this.B01.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B01.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B01.ForeColor = System.Drawing.Color.Red;
+            this.B01.Location = new System.Drawing.Point(387, 144);
+            this.B01.Name = "B01";
+            this.B01.Size = new System.Drawing.Size(127, 94);
+            this.B01.TabIndex = 1;
+            this.B01.UseVisualStyleBackColor = false;
+            this.B01.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // B02
+            // 
+            this.B02.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B02.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B02.ForeColor = System.Drawing.Color.Red;
+            this.B02.Location = new System.Drawing.Point(520, 144);
+            this.B02.Name = "B02";
+            this.B02.Size = new System.Drawing.Size(127, 94);
+            this.B02.TabIndex = 2;
+            this.B02.UseVisualStyleBackColor = false;
+            this.B02.Click += new System.EventHandler(this.B02_Click);
+            // 
+            // B10
+            // 
+            this.B10.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B10.ForeColor = System.Drawing.Color.Red;
+            this.B10.Location = new System.Drawing.Point(254, 244);
+            this.B10.Name = "B10";
+            this.B10.Size = new System.Drawing.Size(127, 94);
+            this.B10.TabIndex = 3;
+            this.B10.UseVisualStyleBackColor = false;
+            this.B10.Click += new System.EventHandler(this.B10_Click);
+            // 
+            // B11
+            // 
+            this.B11.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B11.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B11.ForeColor = System.Drawing.Color.Red;
+            this.B11.Location = new System.Drawing.Point(387, 244);
+            this.B11.Name = "B11";
+            this.B11.Size = new System.Drawing.Size(127, 94);
+            this.B11.TabIndex = 4;
+            this.B11.UseVisualStyleBackColor = false;
+            this.B11.Click += new System.EventHandler(this.B11_Click);
+            // 
+            // B12
+            // 
+            this.B12.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B12.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B12.ForeColor = System.Drawing.Color.Red;
+            this.B12.Location = new System.Drawing.Point(520, 244);
+            this.B12.Name = "B12";
+            this.B12.Size = new System.Drawing.Size(127, 94);
+            this.B12.TabIndex = 5;
+            this.B12.UseVisualStyleBackColor = false;
+            this.B12.Click += new System.EventHandler(this.B12_Click);
+            // 
+            // B20
+            // 
+            this.B20.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B20.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B20.ForeColor = System.Drawing.Color.Red;
+            this.B20.Location = new System.Drawing.Point(254, 344);
+            this.B20.Name = "B20";
+            this.B20.Size = new System.Drawing.Size(127, 94);
+            this.B20.TabIndex = 6;
+            this.B20.UseVisualStyleBackColor = false;
+            this.B20.Click += new System.EventHandler(this.B20_Click);
+            // 
+            // B21
+            // 
+            this.B21.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B21.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B21.ForeColor = System.Drawing.Color.Red;
+            this.B21.Location = new System.Drawing.Point(387, 344);
+            this.B21.Name = "B21";
+            this.B21.Size = new System.Drawing.Size(127, 94);
+            this.B21.TabIndex = 7;
+            this.B21.UseVisualStyleBackColor = false;
+            this.B21.Click += new System.EventHandler(this.B21_Click);
+            // 
+            // B22
+            // 
+            this.B22.BackColor = System.Drawing.SystemColors.InfoText;
+            this.B22.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Italic);
+            this.B22.ForeColor = System.Drawing.Color.Red;
+            this.B22.Location = new System.Drawing.Point(520, 344);
+            this.B22.Name = "B22";
+            this.B22.Size = new System.Drawing.Size(127, 94);
+            this.B22.TabIndex = 8;
+            this.B22.UseVisualStyleBackColor = false;
+            this.B22.Click += new System.EventHandler(this.B22_Click);
+            // 
+            // TBply1
+            // 
+            this.TBply1.BackColor = System.Drawing.Color.IndianRed;
+            this.TBply1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBply1.Location = new System.Drawing.Point(206, 73);
+            this.TBply1.Name = "TBply1";
+            this.TBply1.Size = new System.Drawing.Size(175, 31);
+            this.TBply1.TabIndex = 9;
+            this.TBply1.Text = "Player 1";
+            this.TBply1.TextChanged += new System.EventHandler(this.TBply1_TextChanged);
+            // 
+            // TBply2
+            // 
+            this.TBply2.BackColor = System.Drawing.Color.Turquoise;
+            this.TBply2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.TBply2.Location = new System.Drawing.Point(520, 73);
+            this.TBply2.Name = "TBply2";
+            this.TBply2.Size = new System.Drawing.Size(175, 31);
+            this.TBply2.TabIndex = 10;
+            this.TBply2.Text = "Player 2";
+            this.TBply2.TextChanged += new System.EventHandler(this.TBply2_TextChanged);
+            // 
+            // Lwinner
+            // 
+            this.Lwinner.AutoSize = true;
+            this.Lwinner.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lwinner.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Lwinner.Location = new System.Drawing.Point(339, 482);
+            this.Lwinner.Name = "Lwinner";
+            this.Lwinner.Size = new System.Drawing.Size(0, 35);
+            this.Lwinner.TabIndex = 11;
+            this.Lwinner.Click += new System.EventHandler(this.Lwinner_Click);
+            // 
+            // Breplay
+            // 
+            this.Breplay.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Breplay.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Breplay.Location = new System.Drawing.Point(387, 13);
+            this.Breplay.Name = "Breplay";
+            this.Breplay.Size = new System.Drawing.Size(127, 36);
+            this.Breplay.TabIndex = 12;
+            this.Breplay.Text = "Replay";
+            this.Breplay.UseVisualStyleBackColor = false;
+            this.Breplay.Click += new System.EventHandler(this.Breplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.Breplay);
+            this.Controls.Add(this.Lwinner);
+            this.Controls.Add(this.TBply2);
+            this.Controls.Add(this.TBply1);
+            this.Controls.Add(this.B22);
+            this.Controls.Add(this.B21);
+            this.Controls.Add(this.B20);
+            this.Controls.Add(this.B12);
+            this.Controls.Add(this.B11);
+            this.Controls.Add(this.B10);
+            this.Controls.Add(this.B02);
+            this.Controls.Add(this.B01);
+            this.Controls.Add(this.B00);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "X-O by ILYAS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B00;
+        private System.Windows.Forms.Button B01;
+        private System.Windows.Forms.Button B02;
+        private System.Windows.Forms.Button B10;
+        private System.Windows.Forms.Button B11;
+        private System.Windows.Forms.Button B12;
+        private System.Windows.Forms.Button B20;
+        private System.Windows.Forms.Button B21;
+        private System.Windows.Forms.Button B22;
+        private System.Windows.Forms.TextBox TBply1;
+        private System.Windows.Forms.TextBox TBply2;
+        private System.Windows.Forms.Label Lwinner;
+        private System.Windows.Forms.Button Breplay;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+
+    {
+        static public int compt = 0;
+        public String Player1 = "Player 1", Player2 = "Player 2";
+        public int[,]p = new int[3,3];
+        public Boolean changePly1 = false, changePly2 = false; 
+        public Form1()
+        {
+            InitializeComponent();
+            initialiser(); 
+
+
+        }
+
+        public void initialiser()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    p[i, j] = -(i + 3 * j + 1);
+                }
+            }
+        }
+
+        public Boolean WhoWins() {
+            for(int i =0; i<3; i++)
+            {
+                if (p[i,0] == p[i,1] && p[i,0] == p[i,2])  return true;
+                if (p[0, i] == p[1, i] && p[0, i] == p[2, i]) return true;
+            }
+            if (p[0, 0] == p[1,1] && p[0,0] == p[2,2]) return true;
+            if (p[0, 2] == p[1, 1] && p[1, 1] == p[2, 0]) return true;
+            return false; 
+
+        }
+
+        public void writeXO(Button button, int c, int x, int y)
+        {
+
+            if (!WhoWins()) { 
+
+                if (c % 2 == 1)
+            {
+                button.ForeColor = Color.Red;
+                button.Text = "X";
+                p[x, y] = 1;
+
+                if (WhoWins()) {  Lwinner.Text = Player1 + " wins"; this.BackColor =Color.Red; };
+
+            }
+            else
+            { button.ForeColor = Color.Blue;
+                button.Text = "O";
+                p[x, y] = 0;
+                if (WhoWins()) {  Lwinner.Text = Player2 + " wins"; this.BackColor = Color.Blue; }
+
+
+
+            }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B00, compt,0,0);
+            B00.Click -= button1_Click;
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B01, compt,0,1);
+            B01.Click -= button1_Click_1;
+        }
+
+        private void B02_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B02, compt,0,2);
+            B02.Click -= B02_Click;
+        }
+
+        private void B10_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B10, compt,1,0);
+            B10.Click -= B10_Click;
+
+        }
+
+        private void B11_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B11, compt,1,1);
+            B11.Click -= B11_Click;
+        }
+
+        private void B12_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B12, compt,1,2);
+            B12.Click -= B12_Click;
+
+        }
+
+        private void B20_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B20, compt,2,0);
+            B20.Click -= B20_Click;
+
+        }
+
+        private void B21_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B21, compt,2,1);
+            B21.Click -= B21_Click;
+
+        }
+
+        private void B22_Click(object sender, EventArgs e)
+        {
+            compt++;
+            writeXO(B22, compt,2,2);
+            B22.Click -= B22_Click;
+        }
+
+        private void TBply2_TextChanged(object sender, EventArgs e)
+        {
+            if (changePly2 == false)
+            {
+                changePly2= true;
+                TBply2.Text = TBply2.Text.Replace("Player 2", "");
+                TBply2.SelectionStart = TBply2.TextLength;
+
+            }
+            if (TBply2.Text != "") Player2= TBply2.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Breplay_Click(object sender, EventArgs e)
+        {
+            compt =0; 
+            Lwinner.Text = "";
+            B00.Text = ""; B01.Text = ""; B02.Text = ""; B10.Text = ""; B11.Text = ""; B12.Text = ""; B20.Text = ""; B21.Text = ""; B22.Text = "";
+            B00.Click += button1_Click; ; B01.Click += button1_Click_1; B02.Click += B02_Click;
+            B10.Click += B10_Click; B11.Click += B11_Click; B12.Click += B12_Click;
+            B20.Click += B20_Click; B21.Click += B21_Click; B22.Click += B22_Click;
+            this.BackColor = Color.White;
+            initialiser();
+        }
+
+        private void Lwinner_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBply1_TextChanged(object sender, EventArgs e)
+        {
+            if(changePly1==false)
+            {
+                changePly1 = true;
+                TBply1.Text = TBply1.Text.Replace("Player 1" , "");
+                TBply1.SelectionStart = TBply1.TextLength;
+
+            }
+           if(TBply1.Text!="") Player1 = TBply1.Text;
+        }
+    }
+}
